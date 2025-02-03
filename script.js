7ec8418f260b5d3201f4f004cef587376eef9d9e
Neven <nehuenlentini@gmail.com>
2025-02-03 14:41:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
 let menuVisible = false;
-//Función que oculta o muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList ="";
+
+// Función que oculta o muestra el menú
+function mostrarOcultarMenu() {
+    const nav = document.getElementById("nav");
+    
+    if (menuVisible) {
+        nav.classList.remove("responsive"); // Elimina la clase "responsive"
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList ="responsive";
+    } else {
+        nav.classList.add("responsive"); // Añade la clase "responsive"
         menuVisible = true;
     }
 }
 
-function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
-    document.getElementById("nav").classList = "";
+function seleccionar() {
+    // Oculto el menú una vez que selecciono una opción
+    const nav = document.getElementById("nav");
+    nav.classList.remove("responsive"); // Elimina la clase "responsive"
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
-}
+document.querySelector('.nav-responsive').addEventListener('click, mostrarOcultarMenu');
+    document.querySelector('.nav-responsive').addEventListener('touchstart', mostrarOcultarMenu);
 
+   
+   
 
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+emailjs.init({
+    publicKey: 'OptkNRo4EPsfrpJAJ',
+  });
+  emailjs.sendForm('service_smynix9', 'template_1lysp8i', this);
